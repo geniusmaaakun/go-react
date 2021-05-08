0986e96fdc01e271f4fed4a37af6b437f69dfa7f
Trevor Sawler <trevor.sawler@gmail.com>
2021-05-08 13:38:01 -0300

    New components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import AppContent from './AppContent';
 import AppFooter from './footer';
 import './index.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div>
         <h1>Hello, world!</h1>
+        <AppContent />
         <AppFooter />
       </div>
     );
